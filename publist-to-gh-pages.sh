60f10e354c7b9e6178c5a24a9e483f0a022d2292
lcx4 <lcx4@meitu.com>
2019-03-13 16:54:20 +0800

    test

--- publist-to-gh-pages.sh
+++ publist-to-gh-pages.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -ev
-git clone https://${GH_REF} .deploy_git
-cd .deploy_git
-git checkout master
-cd ../
-mv .deploy_git/.git/ ./public/
+#git clone https://${GH_REF} .deploy_git
+#cd .deploy_git
+#git checkout master
+#cd ../
+#mv .deploy_git/.git/ ./public/
 cd ./public
 git config user.name  "luochenxi"
 git config user.email "chenxiluo520@qq.com" 
 # add commit timestamp
+git init
 git add .
 git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
 git push --force --quiet "https://${Travis_gh_token}@${GH_REF}" master:master
